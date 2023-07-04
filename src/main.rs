3093a47332af5a24a90919c3a0fcb9dbff859762
markzuckerbergas <markzuckerbergas@protonmail.com>
2023-07-04 12:52:53 -0600

    Added camera movement when moving the mouse to the edges

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 //! A simple 3D scene with light shining over a osrs player model.
-//! //! This example illustrates how to load a glTF model with animations and play them back.
+//! Simple animation control and camera movement.
 //!
 //! Controls:
 //! - return: start / change animation
-//! - spacebar: play / pause
-//! - arrow up / down: speed up / slow down animation playback
-//! - arrow left / right: seek backward / forward
+//! - spacebar: play / pause animation
+//! - arrows: move camera
 
+use bevy::input::mouse::MouseWheel;
 use bevy::{prelude::*, render::camera::ScalingMode};
 use std::time::Duration;
 
@@ -17,6 +17,7 @@ fn main() {
         .add_system(setup_scene_once_loaded)
         .add_system(keyboard_animation_control)
         .add_system(keyboard_camera_movement)
+        .add_system(mouse_camera_movement)
         .run();
 }
 
@@ -75,12 +76,6 @@ fn setup(
         transform: Transform::from_xyz(5.0, 5.0, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
-
-    println!("Animation controls:");
-    println!("  - spacebar: play / pause");
-    println!("  - arrow up / down: speed up / slow down animation playback");
-    println!("  - arrow left / right: seek backward / forward");
-    println!("  - return: change animation");
 }
 
 fn setup_scene_once_loaded(
@@ -135,6 +130,55 @@ fn keyboard_animation_control(
                 animations.0[*current_animation].clone_weak(),
                 Duration::from_millis(250),
             );
+        }
+    }
+}
+
+fn mouse_camera_movement(
+    mut cursor_moved_events: EventReader<CursorMoved>,
+    mut mouse_wheel_events: EventReader<MouseWheel>,
+    mut camera: Query<&mut Transform, With<Camera>>,
+    mut windows: Query<&mut Window>,
+) {
+    for event in cursor_moved_events.iter() {
+        // Camera needs to move when the mouse is near the edge of the screen
+        // 1) First we need to get the size of the window
+        let window = windows.single_mut();
+        let scale_factor = window.resolution.scale_factor() as f32;
+        let physical_width = window.resolution.physical_width() as f32;
+        let physical_height = window.resolution.physical_height() as f32;
+
+        let actual_resolution = Vec2::new(
+            physical_width / scale_factor,
+            physical_height / scale_factor,
+        );
+
+        // 2) Then we need to get the mouse position
+        let mouse_position = event.position;
+
+        // 3) Then we need to get the center of the screen
+        let center = actual_resolution / 2.0;
+
+        // 4) Then we need to get the difference between the mouse position and the center
+        let difference = mouse_position - center;
+
+        // 5) Then we need to scale the difference based on the size of the window
+        let scaled_difference = difference / actual_resolution;
+
+        // 6) Then we need to move the camera based on the difference
+        // 6.1) Only move the camera if the mouse is near the edge of the screen
+        if scaled_difference.x.abs() > 0.48 || scaled_difference.y.abs() > 0.48 {
+            for mut transform in camera.iter_mut() {
+                transform.translation +=
+                    Vec3::new(scaled_difference.x * 0.1, scaled_difference.y * 0.1, 0.0);
+            }
+        }
+    }
+
+    for event in mouse_wheel_events.iter() {
+        // Handle zoom
+        for mut transform in camera.iter_mut() {
+            transform.scale *= 1.0 + -event.y / 20.0;
         }
     }
 }
